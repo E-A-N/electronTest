45d39be27f517bf48d26366fe03ed9d3b973dd5c
Cyborg_Ean <ean.director@gmail.com>
2018-08-06 07:01:42 -0700

    testing out DOM stuff

--- client/js/addButton.js
+++ client/js/addButton.js
@@ -1,5 +1,15 @@
 const adder = (amount = 1) => {
+
+    var action = amount >= 0 ? "added" : "subtracted";
     var cnt = document.getElementById("countText");
     cnt.innerHTML = JSON.parse(cnt.innerHTML) + amount;
     console.log(cnt.innerHTML);
+
+    var msg = document.getElementById("msg");
+    msg.innerHTML = amount + " has been " + action + " to the counter!"
+
+    //eanDebug NOTE: make this sort of timer external so multiple things aren't resetting DOM message
+    setTimeout(function(){
+        msg.innerHTML = "Current Message!";
+    },2000)
 }
