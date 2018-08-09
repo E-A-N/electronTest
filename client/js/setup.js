9473549c2e7930ee86932a578a9072168561eeec
Cyborg_Ean <ean.director@gmail.com>
2018-08-09 07:15:17 -0700

    using path module to make app cross compatible, going work on dynamically adding nodes to DOM

--- client/js/setup.js
+++ client/js/setup.js
@@ -1,7 +1,11 @@
+const path      = require("path");
 const liveTyper = require("./client/js/liveType");
-const adder =     require("./client/js/addButton");
+const adder     = require("./client/js/addButton");
+const nodePlus  = require("./client/js/nodeAdd");
 
+console.log(path);
 const start = function() {
     liveTyper();
     adder();
+    nodePlus();
 }
