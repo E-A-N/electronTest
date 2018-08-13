6da039c712800f0630c9c6356df2c429100592f3
Cyborg_Ean <ean.director@gmail.com>
2018-08-13 06:54:19 -0700

    working on modular add/subtract button

--- client/js/nodeAdd.js
+++ client/js/nodeAdd.js
@@ -1,3 +1,18 @@
 module.exports = () => {
-    console.log("Doc is:", document.body);
+
+    return function(type, id){
+        console.log("Doc is:", document.body);
+        const node = document.createElement("div");
+        const label = document.createElement("span");
+        switch(type){
+            case "counter":
+                label.innerHTML = "Counter: ";
+                const addBtn = document.create("button");
+                const subtractBtn = document.create("button");
+            break;
+        }
+
+
+        document.body.append(node);
+    }
 }
