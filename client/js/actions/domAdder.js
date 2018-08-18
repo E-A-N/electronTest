3cf3a9de3369bbbf989b58d14973922c0e617626
Cyborg_Ean <ean.director@gmail.com>
2018-08-17 19:55:47 -0700

    refactoring

---
+++ client/js/actions/domAdder.js
@@ -0,0 +1,6 @@
+module.exports = (node, postNode, amount = 0) => {
+    const action = amount >= 0 ? "added" : "subtracted";
+    node.innerHTML = JSON.parse(node.innerHTML) + amount;
+    console.log(node.innerHTML);
+    postNode.innerHTML = amount + " has been " + action + " to the counter!"
+}
--- client/js/addButton.js
+++ client/js/addButton.js
@@ -1,13 +1,9 @@
 module.exports = () => {
     const nodeBuilder = require("./nodeAdd")();
+    const domAdder = require("./actions/domAdder");
     const msgNode = document.getElementById("countText");
     const noteNode = document.getElementById("msg");
-    const domAdder = (node, postNode, amt = 0) => {
-        const action = amt >= 0 ? "added" : "subtracted";
-        node.innerHTML = JSON.parse(node.innerHTML) + amt;
-        console.log(node.innerHTML);
-        postNode.innerHTML = amt + " has been " + action + " to the counter!"
-    }
+
 
     const addButton = document.getElementById("addBtn");
     const subtractBtn = document.getElementById("subtractBtn");
