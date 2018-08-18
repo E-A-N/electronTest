b9de635486c452826ca49c0f9dfc8aff976d66fd
Cyborg_Ean <ean.director@gmail.com>
2018-08-17 19:46:50 -0700

    fixing up architecture

--- client/js/addButton.js
+++ client/js/addButton.js
@@ -1,25 +1,24 @@
 module.exports = () => {
     const nodeBuilder = require("./nodeAdd")();
-    const domAdder = (amt = 0) => {
+    const msgNode = document.getElementById("countText");
+    const noteNode = document.getElementById("msg");
+    const domAdder = (node, postNode, amt = 0) => {
         const action = amt >= 0 ? "added" : "subtracted";
-        const cnt = document.getElementById("countText");
-        cnt.innerHTML = JSON.parse(cnt.innerHTML) + amt;
-        console.log(cnt.innerHTML);
-
-        const msg = document.getElementById("msg");
-        msg.innerHTML = amt + " has been " + action + " to the counter!"
+        node.innerHTML = JSON.parse(node.innerHTML) + amt;
+        console.log(node.innerHTML);
+        postNode.innerHTML = amt + " has been " + action + " to the counter!"
     }
 
     const addButton = document.getElementById("addBtn");
     const subtractBtn = document.getElementById("subtractBtn");
     addButton.onclick = () => {
-        domAdder(1);
+        domAdder(msgNode, noteNode, 1);
         let config = {type: 'counter', amount: 1};
         var n = nodeBuilder(config);
         console.log(n);
     }
     subtractBtn.onclick = () => {
-        domAdder(-1);
+        domAdder(msgNode,noteNode, -1);
         var child = document.body.lastElementChild;
         if(child.getAttribute("class") === "counterBtn"){
             console.log(child);
--- client/js/counterBtn.js
+++ client/js/counterBtn.js
@@ -1,11 +1,6 @@
 module.exports = (type, target, amount) => {
-    const addBtn = document.createElement("button");
+    let addBtn = document.createElement("button");
     addBtn.innerHTML = type;
     addBtn = require("./actions/countNumber")(addBtn, target, amount);
-    // addBtn.onclick = () => {
-    //     const content = JSON.parse(target.innerHTML) + amount;
-    //     console.log("I am clicked!!",content);
-    //     target.innerHTML = content;
-    // }
     return addBtn;
 }
