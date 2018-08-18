b1189ddcf426de1e5f0a6737d5b68ef4b17549f2
Cyborg_Ean <ean.director@gmail.com>
2018-08-17 19:37:45 -0700

    quick update

--- client/js/counterBtn.js
+++ client/js/counterBtn.js
@@ -1,7 +1,7 @@
 module.exports = (type, target, amount) => {
     const addBtn = document.createElement("button");
     addBtn.innerHTML = type;
-    //addBtn = require()
+    addBtn = require("./actions/countNumber")(addBtn, target, amount);
     // addBtn.onclick = () => {
     //     const content = JSON.parse(target.innerHTML) + amount;
     //     console.log("I am clicked!!",content);
