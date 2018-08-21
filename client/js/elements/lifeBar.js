9e16f50c4ab1d69396acccf766f7cfe88bba9c92
Cyborg_Ean <ean.director@gmail.com>
2018-08-21 07:08:27 -0700

    working on adding life node to dom

--- client/js/elements/lifeBar.js
+++ client/js/elements/lifeBar.js
@@ -1,12 +1,16 @@
 module.exports = (name, type, props) => {
     const canvas = document.createElement("canvas");
     const ctx = canvas.getContex("2d");
-    const x = props.x ? props.x : 0;
-    const y = props.y ? props.y : 0;
-    const width = props.width ? props.width : 100;
+
+    const x      = props.x ? props.x : 0;
+    const y      = props.y ? props.y : 0;
+    const width  = props.width  ? props.width  : 100;
     const height = props.height ? props.height : 100;
-    //canvas.innerHTML = type
-    //addBtn = require("../actions/countNumber")(addBtn, target, amount);
+    const color  = props.color  ? props.color  : "green";
+
+    canvas.width  = width;
+    canvas.hegiht = height;
+    
     ctx.fillStyle = color;
     ctx.fillRect(x, y, width, height)
     return canvas;
