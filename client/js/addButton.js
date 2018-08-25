167d52e618a7261eb57805c977d7d4b65cfe8d9d
Cyborg_Ean <ean.director@gmail.com>
2018-08-25 00:33:27 -0700

    implemented action granter

--- client/js/addButton.js
+++ client/js/addButton.js
@@ -1,19 +1,20 @@
 module.exports = () => {
     const nodeBuilder = require("./nodeAdd")();
-    const parseAdd = require("./actions/parseAdd");
+    const parseAdd      = require("./actions/parseAdd");
+    const actionGranter = require("./construction/actionGrant");
     const msgNode = document.getElementById("countText");
     const noteNode = document.getElementById("msg");
 
-
-    const addButton = document.getElementById("counterAdd");
+    const addId = document.getElementById("counterAdd");
+    const addButton = actionGranter(addId, "counterBtnAdd");
     const subtractId = document.getElementById("nodeDelete");
     const subtractBtn = document.getElementById("nodeDelete");
-    addButton.onclick = () => {
-        parseAdd(msgNode, noteNode, 1);
-        let config = {type: 'counter', amount: 1};
-        var n = nodeBuilder(config);
-        console.log(n);
-    }
+    // addButton.onclick = () => {
+    //     parseAdd(msgNode, noteNode, 1);
+    //     let config = {type: 'counter', amount: 1};
+    //     var n = nodeBuilder(config);
+    //     console.log(n);
+    // }
 
     subtractBtn.onclick = () => {
         parseAdd(msgNode,noteNode, -1);
--- client/js/construction/actionGrant.js
+++ client/js/construction/actionGrant.js
@@ -1,26 +1,28 @@
 module.exports = () => {
+        (node, type) => {
 
-    return (node) => {
-
-        switch(node.type){
-            case "counterBtnAdd":
-                node.onclick = () => {
-                    domAdder(msgNode, noteNode, 1);
-                    let config = {type: 'counter', amount: 1};
-                    var n = nodeBuilder(config);
-                    console.log(n);
-                }
-            break;
-
-            case "btnDelete":
-                node.onclick = () => {
-                    var child = document.body.lastElementChild;
-                    if(child.getAttribute("class") === "counterBtn"){
-                        console.log(child);
-                        document.body.removeChild(child);
+            switch(type){
+                case "counterBtnAdd":
+                    const nodeBuilder = require("../nodeAdd");
+                    node.onclick = () => {
+                        //domAdder(msgNode, noteNode, 1);
+                        let config = {type: 'counter', amount: 1};
+                        var n = nodeBuilder(config);
+                        console.log(n);
                     }
-                }
-            break;
-        }
+                break;
+
+                case "btnDelete":
+                    node.onclick = () => {
+                        var child = document.body.lastElementChild;
+                        if(child.getAttribute("class") === "counterBtn"){
+                            console.log(child);
+                            document.body.removeChild(child);
+                        }
+                    }
+                break;
+            }
+
+            return node;
     }
 }
